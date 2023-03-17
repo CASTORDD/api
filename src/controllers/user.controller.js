82cce1e148234f1a650ea1b2b6e1f8c392fc8f69
CASTORDD <CASTORDD@users.noreply.github.com>
2023-03-17 14:51:21 -0300

    Start project

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,49 @@
+import bcrypt from "bcrypt"
+import { createUser, getAll, getById, updateUser, deleteuser } from "../repositorys/user.repository"
+import { userValidation } from "../validations/user.validatio"
+
+export const create = async (req, res) => {
+  try {
+    await userValidation.validate(req.body)
+
+    const hashPassword = await bcrypt.hash(req.body.password, 10)
+    req.body.password = hashPassword
+    const user = await createUser(req.body)
+    res.status(200).send(user)
+  } catch (error) {
+    res.status(400).send(error)
+  }
+}
+
+export const get = async (req, res) => {
+  try {
+    const users = await getAll()
+    res.status(200).send(users)
+  } catch (error) {
+    res.status(400).send(error)
+  }
+}
+
+export const getId = async (req, res) => {
+  try {
+    const user = await getById(Number(req.params.id))
+    res.status(200).send(user)
+  } catch (error) {
+    res.status(400).send(error)
+  }
+}
+
+export const update = async (req, res) => {
+  try {
+    const user = await updateUser(Number(req.params
+      .id), req.body)
+
+    res.status(200).send(user)
+  } catch (error) {
+    res.status(400).send(error)
+  }
+}
+
+export const remove = async (req, res) => {
+
+}
---
+++ src/routes/user.routes.js
@@ -0,0 +1,10 @@
+import { create, get, getId, update } from "../controllers/user.controller"
+
+const userRoutes = (app) => {
+  app.post("/user", create)
+  app.get("/user", get)
+  app.get("/user/:id", getId)
+  app.put("/user/:id", update)
+}
+
+export default userRoutes
